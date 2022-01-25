69c7bc40efb2b52fda3a36464a27d4fd03668797
yashNarodia <ynarodia@gmail.com>
2022-01-25 11:11:26 +0530

    Modified Assignment 4

--- assignment4.py
+++ assignment4.py
@@ -1,10 +1,6 @@
 #Find runner-up from given list
 # GITHUB-LINK->https://github.com/yashNarodia/PIP_Assignments.git
 
-from os import scandir
-from traceback import print_tb
-
-
 # k = int(input())
 ScoreList = list(map(int,input().split()))
 ScoreSet = set(ScoreList)
